bfc0d2e676ac39ad4e830a653ee7a96e99bfb99b
gamari <gamari4552@gmail.com>
2023-12-30 09:32:56 +0900

    CodeEditorの著者以外は編集できないように変更

--- app/(private)/dashboard/codes/[code_id]/edit/_components/CodeEditor.tsx
+++ app/(private)/dashboard/codes/[code_id]/edit/_components/CodeEditor.tsx
@@ -7,12 +7,20 @@ import { CodeEditorProvider } from "@/src/contexts/CodeEditorProvider";
 import { CodeDetail } from "@/src/types";
 import { Flex } from "@/src/components/atoms/containers/Flex";
 import { CodeEditorContent } from "./content/CodeEditorContent";
+import { useSupabase } from "@/src/contexts/SupabaseProvider";
+import { NoContent } from "@/src/components/molecules/displays/no-content";
 
 interface Props {
   code: CodeDetail;
 }
 
 export const CodeEditor: FunctionComponent<Props> = ({ code }: Props) => {
+  const { authUser } = useSupabase();
+
+  if (code?.user_id !== authUser?.id) {
+    return <NoContent>編集対象がありません</NoContent>;
+  }
+
   return (
     <CodeEditorProvider code={code}>
       <Flex gap={16}>
