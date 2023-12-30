2d32f3acb64a18f07a2f17bab57a9f7979b02ea7
gamari <gamari4552@gmail.com>
2023-12-31 07:50:18 +0900

    done機能を追加

--- app/(public)/codes/more/MorePage.tsx
+++ app/(public)/codes/more/MorePage.tsx
@@ -10,7 +10,8 @@ import { Center } from "@/src/components/atoms/containers/Center";
 import { LoadingContainer } from "@/src/components/molecules/loding-container";
 
 export const MorePage = () => {
-  const { codeList, fetchMoreCodeList, loading } = useFetchLatestCodeList();
+  const { codeList, fetchMoreCodeList, loading, isDone } =
+    useFetchLatestCodeList();
 
   return (
     <div className="max-w-5xl w-full mx-auto px-10 lg:px-0 py-24 lg:py-16">
@@ -20,15 +21,17 @@ export const MorePage = () => {
 
       <CodePanelList codes={codeList} />
 
-      <LoadingContainer isLoading={loading} className="mt-20">
-        <Center>
-          <DownIcon
-            onClick={fetchMoreCodeList}
-            size="lg"
-            className="rounded-full border-2 border-gray-500 cursor-pointer hover:text-sky-500"
-          />
-        </Center>
-      </LoadingContainer>
+      {!isDone && (
+        <LoadingContainer isLoading={loading} className="mt-20">
+          <Center>
+            <DownIcon
+              onClick={fetchMoreCodeList}
+              size="lg"
+              className="rounded-full border-2 border-gray-500 cursor-pointer hover:text-sky-500"
+            />
+          </Center>
+        </LoadingContainer>
+      )}
     </div>
   );
 };
