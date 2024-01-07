2334ed8fd92fa1badfa62d5cfea3970b23774d26
gamari <gamari4552@gmail.com>
2024-01-07 10:22:47 +0900

    Preview機能の実装

--- app/(private)/dashboard/codes/[code_id]/edit/_components/content/CodeEditorContentDescription.tsx
+++ app/(private)/dashboard/codes/[code_id]/edit/_components/content/CodeEditorContentDescription.tsx
@@ -5,8 +5,12 @@ import React from "react";
 import { Textarea } from "@/src/components/atoms/forms/textarea";
 import { Heading } from "@/src/components/atoms/texts/heading";
 import { useCodeEditorSelectedFile } from "@/src/hooks/codes/editors/useCodeEditorSelectedFile";
+import { Flex } from "@/src/components/atoms/containers/Flex";
+import { PreviewButton } from "@/src/components/molecules/preview-button";
+import { MarkdownPreviewer } from "@/src/components/molecules/displays/markdown-previewer";
 
 export const CodeEditorContentDescription = () => {
+  const [isPreview, setIsPreview] = React.useState(false);
   const { selectedFile, setDescription } = useCodeEditorSelectedFile();
 
   if (!selectedFile) return;
@@ -14,15 +18,25 @@ export const CodeEditorContentDescription = () => {
   return (
     <div className="mt-6">
       <Heading className="mb-3">コード説明(空欄可)</Heading>
-      <Textarea
-        className="w-full"
-        value={selectedFile?.description || ""}
-        onChange={(e) => {
-          setDescription(e.target.value);
-        }}
-        rows={8}
-        placeholder="上記コードの説明を書いてください（マークダウン形式）"
-      />
+
+      {isPreview ? (
+        <div className="h-[250px] bg-white p-6 border overflow-y-scroll">
+          <MarkdownPreviewer content={selectedFile?.description || ""} />
+        </div>
+      ) : (
+        <Textarea
+          className="w-full"
+          value={selectedFile?.description || ""}
+          onChange={(e) => {
+            setDescription(e.target.value);
+          }}
+          rows={12}
+          placeholder="上記コードの説明を書いてください（マークダウン形式）"
+        />
+      )}
+      <Flex direction="row-reverse" className="mt-3">
+        <PreviewButton isPreview={isPreview} setIsPreview={setIsPreview} />
+      </Flex>
     </div>
   );
 };
