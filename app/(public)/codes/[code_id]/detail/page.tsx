f39c22a91726cea01f40a4f99564fcee19513e3f
gamari <gamari4552@gmail.com>
2024-01-09 02:17:53 +0900

    meta dataの修正

--- app/(public)/codes/[code_id]/detail/page.tsx
+++ app/(public)/codes/[code_id]/detail/page.tsx
@@ -6,6 +6,7 @@ import { NextPage } from "next";
 import { DetailCodeCommentListProvider } from "@/src/contexts/CodeCommentListProvider";
 import { CodeDetailProvider } from "@/src/contexts/CodeDetailProvider";
 import { CodeDetail } from "./_components/code-detail";
+import { actionGetCodeById } from "@/src/actions/codes";
 
 interface Props {
   params: {
@@ -14,6 +15,30 @@ interface Props {
 }
 
 export const revalidate = 0;
+
+export const generateMetadata = async ({
+  params,
+}: {
+  params: {
+    code_id: number;
+  };
+}) => {
+  const code = await actionGetCodeById(params?.code_id);
+
+  return {
+    title: `${code.title}`,
+    description: code.description,
+    openGraph: {
+      title: code.title,
+      description: code.description,
+    },
+    twitter: {
+      title: code.title,
+      card: "summary_large_image",
+      description: code.description,
+    },
+  };
+};
 
 const CodeDetailPage: NextPage<Props> = async ({ params: { code_id } }) => {
   unstable_noStore();
