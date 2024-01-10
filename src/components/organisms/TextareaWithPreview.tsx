c88d19c540f0967bf4c52a1374dacf0757ea6d42
gamari <gamari4552@gmail.com>
2024-01-11 05:35:33 +0900

    comment にプレビュー機能を追加

---
+++ src/components/organisms/TextareaWithPreview.tsx
@@ -0,0 +1,47 @@
+import { cn } from "@/src/libs/utils";
+import React from "react";
+import { MarkdownPreviewer } from "../molecules/displays/markdown-previewer";
+import { Textarea } from "../atoms/forms/textarea";
+import { PreviewButton } from "../molecules/preview-button";
+
+interface Props {
+  className?: string;
+  value: string;
+  setValue: (value: string) => void;
+  onSubmit?: () => void;
+  rows?: number;
+  placeholder?: string;
+}
+
+export const TextareaWithPreview = ({
+  value,
+  setValue,
+  className,
+  onSubmit,
+  rows = 4,
+  placeholder,
+}: Props) => {
+  const [isPreview, setIsPreview] = React.useState(false);
+
+  return (
+    <div className={cn("", className)}>
+      {isPreview ? (
+        <div className="border p-2 h-[400px] overflow-scroll scroll-auto bg-white">
+          <MarkdownPreviewer content={value || ""} />
+        </div>
+      ) : (
+        <Textarea
+          value={value || ""}
+          onChange={(e) => setValue(e.target.value)}
+          placeholder={placeholder}
+          rows={rows}
+          onSubmit={onSubmit}
+        />
+      )}
+
+      <div className="flex flex-row-reverse pt-2">
+        <PreviewButton isPreview={isPreview} setIsPreview={setIsPreview} />
+      </div>
+    </div>
+  );
+};
