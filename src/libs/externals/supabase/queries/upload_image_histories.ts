418da7a9ede57fab27eeb81c5c0b79a130a8cda9
gamari <gamari4552@gmail.com>
2024-01-14 05:00:04 +0900

    総量制限の実装

--- src/libs/externals/supabase/queries/upload_image_histories.ts
+++ src/libs/externals/supabase/queries/upload_image_histories.ts
@@ -4,14 +4,20 @@ import { SupabaseClient } from "@supabase/supabase-js";
 import { QueryOptions, applyQueryOptions } from "../options";
 import { UPLOAD_IMAGE_HISTORY } from "@/src/libs/constants/tables";
 import { convertPostgretErrorToAppErrorMessage } from "../errors";
+import dayjs from 'dayjs';
 
 
 /** 当月のアップロード総量の取得。 */
 export const fetchTotalUploadSizeInMonth = async (userId: string, client: SupabaseClient, options?: QueryOptions) => {
+    const startOfCurrentMonth = dayjs().startOf('month').toISOString();
+    const endOfCurrentMonth = dayjs().endOf('month').toISOString();
+
     let query = client
         .from(UPLOAD_IMAGE_HISTORY)
         .select('file_size')
         .eq('user_id', userId)
+        .gte('upload_timestamp', startOfCurrentMonth)
+        .lte('upload_timestamp', endOfCurrentMonth);
 
     query = applyQueryOptions(query, options);
 
