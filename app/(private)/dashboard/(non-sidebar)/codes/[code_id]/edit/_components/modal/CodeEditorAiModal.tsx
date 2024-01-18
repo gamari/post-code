d549d5b08b897c08ea9636d126c950a6164c9ac9
gamari <gamari4552@gmail.com>
2024-01-18 18:08:22 +0900

    ai具体例機能

--- app/(private)/dashboard/(non-sidebar)/codes/[code_id]/edit/_components/modal/CodeEditorAiModal.tsx
+++ app/(private)/dashboard/(non-sidebar)/codes/[code_id]/edit/_components/modal/CodeEditorAiModal.tsx
@@ -1,13 +1,40 @@
 import React from "react";
 import { useCodeEditorModalContext } from "../../_contexts/CodeEditorModalProvider";
 import { Modal } from "@/src/components/molecules/displays/Modal";
+import { Button } from "@/src/components/atoms/forms/button";
+import { useAi } from "@/src/hooks/ai/useAi";
+import { useCodeEditor } from "../../_hooks/useCodeEditor";
+import { Flex } from "@/src/components/atoms/containers/Flex";
+import { useAlert } from "@/src/hooks/useAlert";
 
 export const CodeEditorAiModal = () => {
+  const { errorAlert } = useAlert();
+  const { createConcrete, loading } = useAi();
+  const { code, addDescription } = useCodeEditor();
   const { isAiOpen, toggleAiModal } = useCodeEditorModalContext();
+
+  const handleOnConcrete = async () => {
+    try {
+        const retText = await createConcrete(code?.description || "");
+        // TODO ファイルで作成するように変更する
+        addDescription(`
+
+
+`)
+        addDescription(retText);
+        toggleAiModal();
+    } catch (e) {
+        errorAlert("エラーが発生しました。", e);
+    }
+  };
 
   return (
     <Modal isOpen={isAiOpen} onClose={toggleAiModal} className="w-[700px]">
-      AIモーダル
+      <Flex direction="column" alignItems="center">
+        <Button onClick={handleOnConcrete} disabled={loading}>
+          AIに具体例を作成してもらう
+        </Button>
+      </Flex>
     </Modal>
   );
 };
--- src/hooks/ai/useAi.ts
+++ src/hooks/ai/useAi.ts
@@ -1,7 +1,9 @@
 import { useEffect, useState } from "react"
+import OpenAI from "openai";
 
 export const useAi = () => {
     const [aiKey, setAiKey] = useState("")
+    const [loading, setLoading] = useState(false)
 
     useEffect(() => {
         // LocalStorageから取得
@@ -16,5 +18,34 @@ export const useAi = () => {
         setAiKey(aiKey)
     }
 
-    return { aiKey, setAiKey, saveAiKey }
+    const createConcrete = async (content: string) => {
+        if (!aiKey) throw new Error("aiKeyが設定されていません");
+        if (!content) throw new Error("内容を入力して下さい。");
+
+        try {
+            setLoading(true)
+
+            const openai = new OpenAI({
+                apiKey: aiKey,
+                dangerouslyAllowBrowser: true
+            });
+            const prompt = `Javaを利用して、以下の記事の内容の具体例を書いてください。
+
+${content}`
+            const completion = await openai.chat.completions.create({
+                messages: [{ role: "user", content: prompt }],
+                model: "gpt-3.5-turbo",
+            });
+
+            if (!completion?.choices?.length) throw new Error("AIとの通信に失敗しました。");
+            const message = completion.choices[0].message.content || "";
+            return message
+        } catch (e) {
+            throw e;
+        } finally {
+            setLoading(false)
+        }
+    }
+
+    return { aiKey, setAiKey, saveAiKey, createConcrete, loading }
 }
