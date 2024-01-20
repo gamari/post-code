16ecb0a576b631b26cfe85838743e4d34d2e0fe2
gamari <gamari4552@gmail.com>
2024-01-20 10:14:46 +0900

    最小の縦をscreenに変更

--- src/components/atoms/containers/container.tsx
+++ src/components/atoms/containers/container.tsx
@@ -7,7 +7,7 @@ interface Props extends ContainerProps {}
 export const Container = ({ children, className }: Props) => {
   return (
     <div
-      className={cn("max-w-7xl w-full mx-auto px-10 lg:px-0 py-16", className)}
+      className={cn("max-w-7xl w-full mx-auto px-10 lg:px-0 py-16 min-h-screen", className)}
     >
       {children}
     </div>
