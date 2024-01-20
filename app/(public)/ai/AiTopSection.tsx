5ac89c711fa0bc909295e5a1f1837529ffb7f680
gamari <gamari4552@gmail.com>
2024-01-20 11:14:53 +0900

    update

--- app/(public)/ai/AiTopSection.tsx
+++ app/(public)/ai/AiTopSection.tsx
@@ -1,3 +1,5 @@
+import React from "react";
+
 import { TagBadge } from "@/src/components/atoms/badges/tag-badge";
 import { Flex } from "@/src/components/atoms/containers/Flex";
 import { Section } from "@/src/components/atoms/containers/section";
@@ -5,7 +7,6 @@ import { Button } from "@/src/components/atoms/forms/button";
 import { Description } from "@/src/components/atoms/texts/description";
 import { Heading } from "@/src/components/atoms/texts/heading";
 import { Card } from "@/src/components/molecules/displays/card";
-import React from "react";
 
 export const AiTopSection = () => {
   return (
@@ -14,7 +15,7 @@ export const AiTopSection = () => {
         <Heading>「AI」ページ</Heading>
         <div>
           {/* TODO ログイン判定をする */}
-          <Button>AI記事を作成</Button>
+          {/* <Button>AI記事を作成</Button> */}
         </div>
       </Flex>
 
